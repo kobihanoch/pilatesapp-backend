f2e89328e30ea1d58145d9352a6d950a2aa2da7e
kobihanoch <kobikobi622@gmail.com>
2025-04-26 14:27:49 +0300

    Change login route method from GET to POST for user authentication

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -4,7 +4,7 @@ import { loginUser, logoutUser } from "../controllers/authController.js";
 const router = Router();
 
 // User Routes
-router.get("/login", loginUser); // Logging in a user
+router.post("/login", loginUser); // Logging in a user
 router.post("/logout", logoutUser); // Logging out a user
 
 export default router;
