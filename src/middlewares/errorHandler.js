794e9df5d90c7ab42d0d8050c1d66f3586c633e7
kobihanoch <kobikobi622@gmail.com>
2025-05-28 12:42:07 +0300

    refactor(ErrorHandler): Choose error track message dor dev. and prod.

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,5 +1,9 @@
 export const errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
+  // Log to dev console error stack
+  // Log to prod console error message
+  console.error(
+    process.env.NODE_ENV == "development" ? err.stack : err.message
+  );
   const statusCode = err.statusCode || 500;
   res.status(statusCode).json({
     success: false,
