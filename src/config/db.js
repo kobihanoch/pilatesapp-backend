3b888b89541c71770e263d91ad436fc573d948b8
kobihanoch <kobikobi622@gmail.com>
2025-07-10 01:12:58 +0300

    refactor(db, redisClient): Remove deprecated mongoose options and standardize Redis key usage

--- src/config/db.js
+++ src/config/db.js
@@ -3,8 +3,6 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       dbName: process.env.DB_NAME,
     });
     console.log("MongoDB connected successfully");
