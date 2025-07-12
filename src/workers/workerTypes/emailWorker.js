5444883c0c7f7ff75ba2531210bd83fe5a31e785
kobihanoch <kobikobi622@gmail.com>
2025-07-13 01:21:50 +0300

    refactor(EmailWorker): Restructure email worker initialization and logging

--- src/workers/workerTypes/emailWorker.js
+++ src/workers/workerTypes/emailWorker.js
@@ -2,23 +2,29 @@ import emailQueue from "../../queues/emailQueue.js";
 import { sendMail } from "../../config/mailer.js";
 
 export const startEmailWorker = async () => {
-  console.log("Email worker is up - ready for sending emails!");
-  emailQueue.process(5, async (job) => {
-    const { to, subject, html } = job.data;
-    try {
-      // Preventing overflowing of emails
-      if (job.data.expiresAt && Date.now() > job.data.expiresAt) {
-        console.log(`⏱️ Skipping expired email to ${to}`);
-        return;
-      }
+  try {
+    // Try to run the worker
+    emailQueue.process(5, async (job) => {
+      const { to, subject, html } = job.data;
+      try {
+        // Preventing overflowing of emails
+        if (job.data.expiresAt && Date.now() > job.data.expiresAt) {
+          console.log(`⏱️ Skipping expired email to ${to}`);
+          return;
+        }
 
-      await sendMail({ to, subject, html });
-      console.log("Email worker: email sent to", to);
-    } catch (e) {
-      console.log(`❌ Failed to send email to ${to}: ${e.message}`);
-      throw e;
-    }
-  });
+        await sendMail({ to, subject, html });
+        console.log("Email worker: email sent to", to);
+      } catch (e) {
+        console.log(`❌ Failed to send email to ${to}: ${e.message}`);
+        throw e;
+      }
+    });
+    console.log("Email worker is up - ready for sending emails!");
+  } catch (e) {
+    console.log("Email worker failed to start:", e.message);
+    throw e;
+  }
 
   return emailQueue; // For shutdown
 };
